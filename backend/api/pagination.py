b30d0b84faf61fea0ceee42234e6a967d6bdb3ad
Olesya Sim <leka4581698@gmail.com>
2023-07-02 20:47:09 +0300

    2456

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -5,6 +5,7 @@ class RecipePagination(PageNumberPagination):
     page_size_query_param = 'limit'
     page_size = 6
 
+
 class RecipeSubscribePagination(PageNumberPagination):
     page_size_query_param = 'limit'
     page_size = 3
