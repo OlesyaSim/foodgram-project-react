b6ec71032fd0fb05e9375d673a35c2b1d9c031d6
Olesya Sim <leka4581698@gmail.com>
2023-07-02 16:16:50 +0300

    подправила валидатор имени пользователя и валидатор емейла-2

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -10,7 +10,7 @@ def validate_user_name(value):
     reg = re.compile(r'^[\w.@+-]+\Z')
     if not reg.match(value):
         raise ValidationError(
-            u'%s Не заполнено обязательное поле или оно заполнено некорректно'
+            u'%s Не заполнено обязательное поле или оно заполнено некорректно!'
         )
     if value.lower() == 'me':
         raise ValidationError(
